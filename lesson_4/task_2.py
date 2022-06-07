fe34f42a5d28b98e8d537222e3879244ffe62269
Valeriy Fokin <k2foxspb@mail.ru>
2022-06-07 16:53:04 +0300

    task_2

--- lesson_4/task_2.py
+++ lesson_4/task_2.py
@@ -1,21 +1,20 @@
 import datetime as dt
-import pprint
-import time
 import requests
 import json
+
 print(dt.datetime.utcnow())
 URL = 'https://www.cbr-xml-daily.ru/daily_json.js'
 response = requests.get(URL)
 dict_json = json.loads(response.text)
-#pprint.pprint(dict_json)
+
 
 
 def valute(val):
-    if val.apper() in dict_json:
+    if True:
         start = dt.datetime.utcnow()
-        res = f"курс на {start.strftime('%H:%M:%S')} равен {dict_json['Valute'][val]['Value']}"
+        res = f"курс на {start.strftime('%H:%M:%S')} равен {dict_json['Valute'][val.upper()]['Value']}"
         return res
-    else:
-        return 'none'
-x = valute(input())
+
+
+x = valute('usd')
 print(x)
