314bc024c5a0396aba26d822f33bd2c38f1ea570
Valeriy Fokin <k2foxspb@mail.ru>
2022-06-28 18:43:03 +0300

    task 1

---
+++ lesson_6/task_1.py
@@ -0,0 +1,23 @@
+import requests
+from collections import Counter
+
+URL = 'https://raw.githubusercontent.com/elastic/examples/master/Common%20Data%20Formats/nginx_logs/nginx_logs'
+
+req = requests.get(URL)
+with open('log', 'a', encoding='utf-8') as f:
+    f.write(req.text)
+
+
+def pars(log_line):
+    remote_addr, log_line = log_line.split(' - - ')
+    other, log_line = log_line.split(']')
+    request_type, log_line = log_line.split(' /')
+    request_type = request_type.lstrip(' "')
+    requested_resource, log_line = log_line.split(' HTTP')
+
+    return remote_addr, request_type, requested_resource
+
+
+with open('log', encoding='utf-8') as f:
+    while True:
+        print(pars(f.readline()))
