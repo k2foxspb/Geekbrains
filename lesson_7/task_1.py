116d8d6cfe365b23cc6b3cea5e65f92fd43a993b
Valeriy Fokin <k2foxspb@mail.ru>
2022-07-07 21:05:15 +0300

    task_1

---
+++ lesson_7/task_1.py
@@ -0,0 +1,13 @@
+import os
+
+
+def add_dir(director, *args):
+    directory_add = os.path.join(director, *args)
+    if not os.path.exists(directory_add):
+        os.makedirs(directory_add)
+
+
+dict_add = ('my_project', 'settings', 'mainapp', 'admin_app', 'auth_app')
+
+for i in range(len(dict_add)):
+    add_dir(dict_add[0], dict_add[i])
